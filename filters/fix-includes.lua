5376c646eb929076993cdcb949ac4269736af716
oncomouse <oncomouse@gmail.com>
2025-08-26 17:01:46 -0500

    feat: change name of include directory

--- filters/fix-includes.lua
+++ filters/fix-includes.lua
@@ -3,12 +3,12 @@ function Para(el)
     if not fn or fn == "schedule.html" then
         return el
     end
-    local fp = io.open("./_includes/" .. fn)
+    local fp = io.open("./includes/" .. fn)
     if not fp then
         io.close(fp)
         return el
     end
-    local new_el = pandoc.CodeBlock("_includes/" .. fn)
+    local new_el = pandoc.CodeBlock("includes/" .. fn)
     new_el.classes = {"include"}
     return new_el
 end
